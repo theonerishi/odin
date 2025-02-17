46c94bb22c20d95018553b55bbf37886055e353c
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-02-17 17:58:03 +0000

    rock paper scissors

---
+++ function-basics/script.js
@@ -0,0 +1,12 @@
+function add7(n) {
+    return n + 7;
+}
+function multiply(n, m) {
+    return n * m;
+}
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+function lastLetter(str) {
+    return str.charAt(str.length - 1);
+}
---
+++ rockpaperscissors/app.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+function getComputerChoice() {
+    int rnd = Math.floor(Math.random() * 3);
+    if
+}
