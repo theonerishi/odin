870319baf89d54d7450545966f2c6865cfb08432
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-06-22 13:35:01 +0100

    rock paper scissors game created

--- rockpaperscissors/app.js
+++ rockpaperscissors/app.js
@@ -1,5 +1,52 @@
 console.log("Hello World!");
 function getComputerChoice() {
     int rnd = Math.floor(Math.random() * 3);
-    if
+    switch(rnd) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "rock";
+    }
+    console.log("Computer choice: " + getComputerChoice());
+}
+function getHumanChoice() {
+    let humanChoice = prompt("Enter rock, paper, or scissors:");
+    console.log("Human choice: " + humanChoice());
+}
+let humanScore = 0;
+let computerScore = 0;
+function playRound() {
+    let humanSelection = getHumanChoice().toLowerCase();
+    let computerSelection = getComputerChoice().toLowerCase();
+    if (humanSelection == "rock" && computerSelection == "paper") {
+        computerScore += 1;
+    } else if (humanSelection == "paper" && computerSelection == "rock") {
+        humanScore += 1;
+    } else if (humanSelection == "paper" && computerSelection == "scissors") {
+        computerScore += 1;
+    } else if (humanSelection == "scissors" && computerSelection == "paper") {
+        humanScore += 1;
+    } else if (humanSelection == "rock" && computerSelection == "scissors") {
+        humanScore += 1;
+    } else if (humanSelection == "scissors" && computerSelection == "rock") {
+        computerScore += 1;
+    } else {
+
+    }
+}
+function playGame() {
+    for (let i = 0; i < 3; i++) {
+        playRound();
+    }
+    if(humanScore > computerScore) {
+        alert("human wins");
+    } else if (computerScore > humanScore) {
+        alert("computer wins");
+    } else {
+        alert("tie");
+    }
 }
