8c4f9e309b8c545ef9a0343766f5be76a69dac70
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-07-26 21:16:27 +0000

    maximal subarray

---
+++ arrays/javascript-info/maximal-subarray/script.js
@@ -0,0 +1,21 @@
+function getMaxSubSum(arr) {
+    let max = -10000;
+    let sum = 0;
+    for (let k = arr.length; k >= 0; k--) {
+        for (let i = 0; i < k; i++) {
+            for (let j = i; j < k; j++) {
+                console.log(`arr[j] is ${arr[j]}`)
+                sum += arr[j];
+            }
+            console.log(`sum is ${sum}`);
+            console.log(`max is ${max}`)
+            if (sum > max) {
+                console.log(`sum is greater than max`);
+                max = sum;
+            }
+            sum = 0;
+        }
+    }
+    alert(max);
+}
+getMaxSubSum([-1, 2, 3, -9]);
