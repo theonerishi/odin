2663fca4678b64ca1a59415121a57dd9f3df58c1
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-08-21 19:04:32 +0000

    adjusted spacing

--- variables-and-operators/script.js
+++ variables-and-operators/script.js
@@ -1,6 +1,6 @@
-console.log(23+97);
-console.log(1+2+3+4+5+6);
-console.log((4+6+9)/77);
+console.log(23 + 97);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4 + 6 + 9) / 77);
 let a = 10;
 console.log(a);
 a = 11;
